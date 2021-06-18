92f9ae331d378357b8691bc9a978ffeaae9e7679
alfio_gn <negrini.a.giorgio@gmail.com>
2021-06-18 19:50:50 +0200

    modified:   app.py

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config.update(
     MAIL_SERVER='smtp.gmail.com',
     MAIL_PORT=465,
-    MAIL_USE_TLS= False,
-    MAIL_USE_SSL=True,
+    MAIL_USE_TLS=True,
+    MAIL_USE_SSL=False,
     MAIL_USERNAME=os.environ['MAIL_USERNAME'],
     MAIL_PASSWORD=os.environ['MAIL_PASSWORD'],
 )
